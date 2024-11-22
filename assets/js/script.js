49aff6678a642cbe8fcc903f6ead124ffcba8623
MusaibAli-delta <musaibshaheen@gmai.com>
2024-11-22 14:02:29 +0500

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".myswiper1", {
+    slidesPerView: 1,
+    spaceBetween: 3,
+    loop: true,
+    // autoplay: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    
+});
