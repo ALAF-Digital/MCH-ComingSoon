3175eef5525bd48025dc32b52410de00b1c1f7d3
Musaib Ali <musaib.ali@alafdigital.com>
2024-11-25 19:46:30 +0500

    Landing Page Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 var swiper = new Swiper(".myswiper1", {
     slidesPerView: 1,
-    spaceBetween: 3,
+    spaceBetween: 8,
+    centeredSlides: true,
     loop: true,
     // autoplay: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 5,
+            },
+           
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            }
+           
+        }
     
 });
